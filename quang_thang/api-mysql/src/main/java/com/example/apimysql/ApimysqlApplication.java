f95264fd86ac3f14d6a718ac0af65beb86d9bd21
devet-git <devet.279@gmail.com>
2023-02-28 09:27:20 +0700

    basic api with mysql

---
+++ quang_thang/api-mysql/src/main/java/com/example/apimysql/ApimysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.apimysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApimysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApimysqlApplication.class, args);
+	}
+
+}
---
+++ quang_thang/api-mysql/src/test/java/com/example/apimysql/ApimysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apimysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
