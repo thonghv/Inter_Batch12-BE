ca7583a1d91c9efef0ceb5af3b2b1b8fe445a70e
devet-git <devet.279@gmail.com>
2023-03-17 15:25:21 +0700

    Init HRM project

---
+++ hr-management-api/src/main/java/com/intern/hrmanagementapi/HrManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.intern.hrmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementApiApplication.class, args);
+	}
+
+}
