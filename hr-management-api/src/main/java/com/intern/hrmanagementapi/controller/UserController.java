090f55ae609e22648dd8c4c063e402a632c28d6d
Duc Anh <leducanh454645@gmail.com>
2023-03-21 14:41:32 +0700

    config Log4j

--- hr-management-api/src/main/java/com/intern/hrmanagementapi/controller/UserController.java
+++ hr-management-api/src/main/java/com/intern/hrmanagementapi/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.intern.hrmanagementapi.controller;
 
 import com.intern.hrmanagementapi.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +18,8 @@ public class UserController {
 
   @GetMapping
   public ResponseEntity<?> getAllUser() {
+    Logger logger = LogManager.getLogger(UserController.class);
+    logger.info("call api get /users");
     return ResponseEntity.ok(userService.getAllUser());
   }
 }
