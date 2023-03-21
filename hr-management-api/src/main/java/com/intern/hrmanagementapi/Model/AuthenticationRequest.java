b878378247f5f1387045e98065048fcec0c1334c
devet-git <devet.279@gmail.com>
2023-03-21 10:23:42 +0700

    validate login

--- hr-management-api/src/main/java/com/intern/hrmanagementapi/Model/AuthenticationRequest.java
+++ hr-management-api/src/main/java/com/intern/hrmanagementapi/Model/AuthenticationRequest.java
@@ -1,6 +1,8 @@
 package com.intern.hrmanagementapi.Model;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,8 +14,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthenticationRequest {
 
-  @NotEmpty
+  @NotEmpty(message = "email can't empty")
+  @Email(message = "Email is invalid")
   private String email;
-  @NotEmpty
+  @NotEmpty(message = "password can not be empty")
+  @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$#!%*?&])[A-Za-z\\d@#$!%*?&]{8,}$", message = "Password requires at least 8 characters and must include lowercase letters, uppercase letters, numbers and special characters")
   private String password;
 }
--- hr-management-api/src/main/java/com/intern/hrmanagementapi/Model/RegisterRequest.java
+++ hr-management-api/src/main/java/com/intern/hrmanagementapi/Model/RegisterRequest.java
@@ -1,5 +1,9 @@
 package com.intern.hrmanagementapi.Model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +15,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+  @NotEmpty(message = "username can not be empty")
+  @Size(min = 5, message = "username requires at least 5 characters")
   private String username;
+  @NotEmpty
+  @Email
   private String email;
+  //  @Size(min = 8, message = "password requires at least 8 characters")
+  @NotEmpty(message = "password can not be empty")
+  @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$#!%*?&])[A-Za-z\\d@#$!%*?&]{8,}$", message = "password requires at least 8 characters and must include lowercase letters, uppercase letters, numbers and special characters")
   private String password;
 }
