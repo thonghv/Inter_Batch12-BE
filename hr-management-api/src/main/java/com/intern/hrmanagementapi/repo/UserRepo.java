f4a8acd4b2ac811b6a0703c60c59add8d83ebcad
devet-git <devet.279@gmail.com>
2023-03-24 14:54:10 +0700

    handle some exception

--- hr-management-api/src/main/java/com/intern/hrmanagementapi/repo/UserRepo.java
+++ hr-management-api/src/main/java/com/intern/hrmanagementapi/repo/UserRepo.java
@@ -11,4 +11,6 @@ public interface UserRepo extends JpaRepository<UserEntity, UUID> {
 
 
   Optional<UserEntity> findByEmail(String email);
+
+  Optional<UserEntity> findByUsername(String username);
 }
