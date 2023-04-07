d6a430c36517a82b3aa67c3c7eb4e43963cb5f06
Duc Anh <leducanh454645@gmail.com>
2023-04-07 20:02:47 +0700

    Complete feature users api

--- hr-management-api/src/main/java/com/intern/hrmanagementapi/controller/UserController.java
+++ hr-management-api/src/main/java/com/intern/hrmanagementapi/controller/UserController.java
@@ -2,16 +2,19 @@ package com.intern.hrmanagementapi.controller;
 
 import com.intern.hrmanagementapi.constant.EndpointConst;
 import com.intern.hrmanagementapi.entity.UserEntity;
+import com.intern.hrmanagementapi.exception.BadRequestException;
 import com.intern.hrmanagementapi.exception.ResourceAlreadyExistsException;
 import com.intern.hrmanagementapi.exception.ResourceNotFoundException;
 import com.intern.hrmanagementapi.logging.LoggerManager;
 import com.intern.hrmanagementapi.mapping.UserMapper;
 import com.intern.hrmanagementapi.model.ChangePasswordRequestDto;
+import com.intern.hrmanagementapi.model.ResetPasswordDto;
 import com.intern.hrmanagementapi.model.UserAddingDto;
 import com.intern.hrmanagementapi.model.UserDto;
 import com.intern.hrmanagementapi.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.UUID;
@@ -19,85 +22,95 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "User", description = "The user API")
 public class UserController {
 
   private final UserMapper userMapper = Mappers.getMapper(UserMapper.class);
   @Autowired
   UserService userService;
-
   @GetMapping
-  public ResponseEntity<?> getAllUser() {
-    LoggerManager.info("call api get /users");
-    List<UserEntity> userEntities = userService.getAllUser();
-    List<UserDto> usersDto = userMapper.userEntityToUserDto(userEntities);
-
-    return ResponseEntity.ok(usersDto);
+  public ResponseEntity<?> getUsers(
+          @RequestParam(name = "username", required = false) String username,
+          @RequestParam(name = "orderBy", defaultValue = "username") String orderBy,
+          @RequestParam(name = "page", defaultValue = "0") int page,
+          @RequestParam(name = "size", defaultValue = "10") int size) throws ResourceNotFoundException{
+    List<UserEntity> users = userService.getUsers(username, orderBy, page, size);
+    List<UserDto> userDto = userMapper.userEntityToUserDto(users);
+    return new ResponseEntity<>(userDto, HttpStatus.OK);
   }
-
   @GetMapping("/{id}")
   public ResponseEntity<?> getUser(@PathVariable String id) throws ResourceNotFoundException {
     LoggerManager.info("call api get /users/id");
-    UserEntity userEntity;
-    try {
-      userEntity = userService.getUser(id);
-    } catch (ResourceNotFoundException e) {
-      return ResponseEntity.ok(ResponseEntity.ok(e.getMessage()));
-    }
-
+    UserEntity userEntity = (UserEntity) userService.getUser(id);
     UserDto userDto = userMapper.userEntityToUserDto(userEntity);
-    return ResponseEntity.ok(userDto);
+    return new ResponseEntity<>(userDto, HttpStatus.OK);
+  }
+  @GetMapping("/active")
+  public ResponseEntity<?> getActiveUsers() throws ResourceNotFoundException{
+    List<UserEntity> userEntityList= userService.getActiveUsers();
+    List<UserDto> userDtoList = userMapper.userEntityToUserDto(userEntityList);
+    return new ResponseEntity<>(userDtoList,HttpStatus.OK);
+  }
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ResourceAlreadyExistsException.class)
+  public ResponseEntity<String> handleResourceAlreadyExistsException(ResourceAlreadyExistsException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+  }
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<Object> handleBadRequestException(BadRequestException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
   }
 
   @PostMapping
-  public ResponseEntity<?> addUser(@RequestBody UserAddingDto user)
-      throws ResourceAlreadyExistsException {
+  public ResponseEntity<?> addUser(@Valid @RequestBody UserAddingDto user) throws ResourceAlreadyExistsException{
     LoggerManager.info("call api Post /users");
     UserEntity userEntity = userMapper.addingUserToUserEntity(user);
-    try {
-      userService.addUser(userEntity);
-    } catch (ResourceAlreadyExistsException e) {
-      return ResponseEntity.ok(ResponseEntity.ok(e.getMessage()));
-    }
-
-    return ResponseEntity.ok(userEntity);
+    userService.addUser(userEntity);
+    return new ResponseEntity<>(userEntity, HttpStatus.OK);
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<?> updateUser(@PathVariable String id, @RequestBody UserAddingDto user)
-      throws ResourceNotFoundException {
+  public ResponseEntity<?> updateUser(@PathVariable String id, @Valid @RequestBody UserAddingDto user) throws ResourceNotFoundException,ResourceAlreadyExistsException {
     LoggerManager.info("call api Put /users");
     UserEntity userEntity = userMapper.addingUserToUserEntity(user);
     userEntity.setId(UUID.fromString(id));
-    try {
-      userService.updateUser(userEntity);
-    } catch (ResourceAlreadyExistsException | ResourceNotFoundException e) {
-      return ResponseEntity.ok(e.getMessage());
-    }
-    return ResponseEntity.ok(userEntity);
+    userService.updateUser(userEntity);
+    return new ResponseEntity<>(userEntity,HttpStatus.OK);
+  }
+  @PutMapping("/{id}/activate")
+  public ResponseEntity<?> activateUser(@PathVariable String id) throws ResourceNotFoundException{
+    UserEntity userEntity = userService.getUser(id);
+    userService.activateUser(userEntity);
+    return new ResponseEntity<>("User has been activated",HttpStatus.OK);
+  }
+  @PutMapping("/{id}/deactivate")
+  public ResponseEntity<?> deactivateUser(@PathVariable String id) throws ResourceNotFoundException{
+    UserEntity userEntity = userService.getUser(id);
+    userService.deactivateUser(userEntity);
+    return new ResponseEntity<>("User has been deactivated",HttpStatus.OK);
+  }
+  @PutMapping("/reset-password")
+  public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordDto resetPasswordDto) throws ResourceNotFoundException, BadRequestException {
+    userService.resetPassword(resetPasswordDto);
+    return new ResponseEntity<>("Password reset successfully",HttpStatus.OK);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity<?> deleteUser(@PathVariable String id) throws ResourceNotFoundException {
     LoggerManager.info("call api Delete /user");
-    try {
-      userService.deleteUser(id);
-      return new ResponseEntity<>("User has been deleted successfully", HttpStatus.OK);
-    } catch (ResourceNotFoundException e) {
-      return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-    }
+    userService.deleteUser(id);
+    return new ResponseEntity<>("User has been deleted successfully", HttpStatus.OK);
   }
-
   @Operation(summary = "Change password", security = {@SecurityRequirement(name = "bearer-key")})
   @PostMapping(EndpointConst.User.CHANGE_PASSWORD)
   public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordRequestDto req) {
